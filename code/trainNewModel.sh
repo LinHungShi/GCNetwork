393668e964da017b65ed7bc6eb01f6441c4cdff3
LinHungShi <u9600114@gmail.com>
2017-09-05 09:54:50 -0400

    delete code and trainNewModel.sh

--- code/trainNewModel.sh
+++
@@ -1,1 +0,0 @@
-srun --pty python code/main.py 2 data/mb_data/data128x256/mb_train128x256.npz  -lspath log/mb_log/log -vdata data/mb_data/data128x256/mb_val128x256.npz --epochs 5 -bs 2
--- trainNewModel.sh
+++
@@ -1,1 +0,0 @@
-srun --pty python code/main.py 2 data/mb_data/mb_train.npz -mspath model/mb_model/mbModel.h5 -lspath log/mb_log/log -vdata data/mb_data/mb_val.npz --epochs 1
